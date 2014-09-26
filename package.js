1c9a81f1d694ea6994af6bdf727286ca9560c9ba
Christopher Stingl <cjstingl@gmail.com>
2014-09-26 13:47:58 -0400

    Fix missing neat files and increment ver

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "wolves:neat",
   summary: "Meteor 0.9.x - A lightweight, semantic grid framework built on top of Bourbon.",
-  version: "0.1.0",
+  version: "0.2.0",
   git: "https://github.com/wolvesio/meteor-neat"
 });
 
@@ -12,6 +12,9 @@ Package.onUse(function(api) {
     "_neat.scss",
     // Helpers
     "_neat-helpers.scss",
+    //Functions
+    "functions/_new-breakpoint.scss",
+    "functions/_private.scss",
     // Grid
     "grid/_private.scss",
     "grid/_box-sizing.scss",
@@ -26,7 +29,11 @@ Package.onUse(function(api) {
     "grid/_to-deprecate.scss",
     "grid/_visual-grid.scss",
     "grid/_display.scss",
-    "grid/_direction.scss"
+    "grid/_direction.scss",
+    //Settings
+    "settings/_disable-warnings.scss",
+    "settings/_grid.scss",
+    "settings/_visual-grid.scss"
   ], 'server', {
     isAsset: true
   });
