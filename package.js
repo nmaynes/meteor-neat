0dbb6c79fb3522d0233524c7bbdc1995b3e04372
Christopher Stingl <cjstingl@gmail.com>
2015-01-27 13:54:15 -0500

    Actually bump ver :derp:

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "wolves:neat",
   summary: "Meteor 0.9.3+ - A lightweight, semantic grid framework built on top of Bourbon.",
-  version: "0.9.0",
+  version: "1.0.0",
   git: "https://github.com/wolvesio/meteor-neat"
 });
 
